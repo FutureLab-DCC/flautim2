302d638fa2b070ca42877a6d48e2321daa3b5106
sufex00 <pedro_h_nr@hotmail.com>
2025-06-12 01:24:35 -0300

    Update Experiment.py

--- flautim2/pytorch/federated/Experiment.py
+++ flautim2/pytorch/federated/Experiment.py
@@ -52,7 +52,7 @@ class Experiment(fl.client.NumPyClient):
         self.epoch_fl = config["server_round"]
         
         #for epochs in range(1, self.epochs+1):
-        values_metrics_train = self.training_loop(self.dataset.dataloader())
+        loss, values_metrics_train = self.training_loop(self.dataset.dataloader())
         for name in values_metrics_train:
                 self.measures.log(self, name, values_metrics_train[name], validation=False, epoch = self.epoch_fl)
                 return_dic[name] = float(values_metrics_train[name])
